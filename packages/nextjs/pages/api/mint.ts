c32942f233bde59de2a7567c101aaa813277be04
Tomas Puricelli <tomasp1189@gmail.com>
2023-11-14 06:55:30 +0100

    Fix cors issue

--- packages/nextjs/pages/api/mint.ts
+++ packages/nextjs/pages/api/mint.ts
@@ -1,4 +1,26 @@
+import Cors from "cors";
 import type { NextApiRequest, NextApiResponse } from "next";
+
+// Initialize the cors middleware
+const cors = Cors({
+  methods: ["GET", "HEAD", "POST"], // Adjust methods according to your needs
+  origin: "*", // This allows all origins. Adjust as needed for security.
+});
+
+// Define a type for the middleware function
+type MiddlewareFn = (req: NextApiRequest, res: NextApiResponse, next: (result: unknown) => void) => void;
+
+// Helper method to enable CORS Requests
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: MiddlewareFn): Promise<unknown> {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: unknown) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
 
 // Define a type for the mint data structure
 type Mint = {
@@ -10,7 +32,9 @@ function getRandomInteger(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Mint[]>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Mint[]>) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
   // Hardcoded data representing random mints
   const randomMints = [
     { address: "0xc455Bc958a7a85F8b8bE0FE724C7bE2C6461C5f0", amount: getRandomInteger(1, 1000) },
