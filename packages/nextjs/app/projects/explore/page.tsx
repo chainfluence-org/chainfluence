79e1e7f8212497b8506f893e2ee48745b2f944ca
Nico <nisaul@protonmail.com>
2023-12-10 16:06:49 -0300

    Fix header and explore logos

--- packages/nextjs/app/projects/explore/page.tsx
+++ packages/nextjs/app/projects/explore/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { getProjects } from "~~/app/actions";
 
 export default async function Page() {
-  const projects = await getProjects();
+  const projects = (await getProjects()) as any[];
 
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 p-10">
@@ -12,7 +12,7 @@ export default async function Page() {
           className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
         >
           <div className="flex-shrink-0">
-            <img className="h-10 w-10 rounded-full" src={project?.logo_url || ""} alt="" />
+            <img className="h-10 w-10 rounded-full" src={project?.twitter_image_url || ""} alt="" />
           </div>
           <div className="min-w-0 flex-1">
             <Link href={`/projects/${project.token_contract_address}`} className="focus:outline-none">
